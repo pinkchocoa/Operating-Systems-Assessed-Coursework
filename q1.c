50d134ed0a425f0511b3f5049486c03f66c389c5
jodie <46685749+pinkchocoa@users.noreply.github.com>
2021-03-07 22:49:29 +0800

    added input functions and also hardcode

--- q1.c
+++ q1.c
@@ -37,40 +37,76 @@ point of time.
 */
 void getResourceInput(int* typeA, int* typeB, int* typeC, int* typeD)
 {
-    while(printf("Enter the number of total instances of resource type A.\n") 
+    while(printf("Enter the number of total instances of resource type A.\n")
     && scanf("%d", typeA) != 1) //this loops as long as the input is invalid
     //aka it will ask again
         scanf("%*[^\n]%*c"); /*clear buffer for next scanf*/
         
-    while(printf("Enter the number of total instances of resource type B.\n") 
+    while(printf("Enter the number of total instances of resource type B.\n")
     && scanf("%d", typeB) != 1) //this loops as long as the input is invalid
     //aka it will ask again
         scanf("%*[^\n]%*c"); /*clear buffer for next scanf*/
         
-    while(printf("Enter the number of total instances of resource type C.\n") 
+    while(printf("Enter the number of total instances of resource type C.\n")
     && scanf("%d", typeC) != 1) //this loops as long as the input is invalid
     //aka it will ask again
         scanf("%*[^\n]%*c"); /*clear buffer for next scanf*/
         
-    while(printf("Enter the number of total instances of resource type D.\n") 
+    while(printf("Enter the number of total instances of resource type D.\n")
     && scanf("%d", typeD) != 1) //this loops as long as the input is invalid
     //aka it will ask again
         scanf("%*[^\n]%*c"); /*clear buffer for next scanf*/
 }
 
-void getArrayInput(int& [PROCESS_NUM][RESOURCE_NUM])
+void getArrayInput(int array[PROCESS_NUM][RESOURCE_NUM])
 {
+    for(int i = 0; i < PROCESS_NUM; i++)
+        for(int j = 0; j < RESOURCE_NUM; j++)
+            while(printf("Enter the number for[%d][%d]", i, j) && 
+            scanf("%d", &array[i][j]) != 1)
+            scanf("%*[^\n]%*c"); /*clear buffer for next scanf*/
 }
 
+void printArray(int array[PROCESS_NUM][RESOURCE_NUM])
+{
+    for(int i = 0; i < PROCESS_NUM; i++)
+    {
+        for(int j = 0; j < RESOURCE_NUM; j++)
+            printf("%d ", array[i][j]);
+        printf("\n");
+    }
+}
 
 int main()
 {
+    //is this the available?
     int typeA = 0, typeB = 0, typeC = 0, typeD = 0;
-    int allocation[PROCESS_NUM][RESOURCE_NUM];
-    int max[PROCESS_NUM][RESOURCE_NUM];
-    getResourceInput(&typeA, &typeB, &typeC, &typeD);
+    int allocation[PROCESS_NUM][RESOURCE_NUM]={
+        {2,1,3,3},
+        {2,3,1,2},
+        {3,3,3,1},
+        {2,1,3,4},
+        {3,2,2,5},
+        {2,1,2,3},
+    };
+    int max[PROCESS_NUM][RESOURCE_NUM]={
+        {7,3,4,5},
+        {8,6,2,1},
+        {9,5,5,6},
+        {6,4,6,3},
+        {8,3,2,4},
+        {8,3,2,3},
+    };
     
+    //these two functions are for user input, for now we will hardcode
+    //so that its easier for us to test shit tho
+    //getResourceInput(&typeA, &typeB, &typeC, &typeD);
+    //getArrayInput(allocation);
+
+    //printArray(allocation);
     //printf("%d,%d,%d,%d", typeA, typeB, typeC, typeD);
+    
+    
     
     
     return 0;
